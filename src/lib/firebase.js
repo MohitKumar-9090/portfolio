dd7514b3ee9f9f9632df9df30d1a832b1aaf7e95
Mohit Kumar <mohit.pandey@gmail.com>
2026-02-27 11:09:19 +0530

    first commit

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyADGktFIj-Bb3Kk8pI3xwTXqCT1FO7GrL0',
+  authDomain: 'portfolio-bf9fd.firebaseapp.com',
+  projectId: 'portfolio-bf9fd',
+  storageBucket: 'portfolio-bf9fd.firebasestorage.app',
+  messagingSenderId: '913793770108',
+  appId: '1:913793770108:web:5b81b82ec9fd6ca0fa9ff9',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { firebase, auth, db };
